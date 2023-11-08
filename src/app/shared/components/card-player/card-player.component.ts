b39e19d73f3a8913a53d72abdee51d20f0fce15e
claudiogit373 <desarrollo.02@torresytorres.com>
2023-11-08 15:59:40 -0500

    Se suben lso cambios correspondientes a creacion de archivos de servicios, paso de datos entre componentes, y login, y suscripciones.

--- src/app/shared/components/card-player/card-player.component.ts
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TrackModel } from '@core//models/tracks.model';
+import { MultimediaService } from '@shared/services/multimedia.service';
 
 @Component({
   selector: 'app-card-player',
@@ -8,10 +9,14 @@ import { TrackModel } from '@core//models/tracks.model';
 })
 export class CardPlayerComponent implements OnInit {
   @Input() mode: 'small' | 'big' = 'small';
-  @Input() track: TrackModel = {_id:0, name:'', album:'', url:'', cover:''};
-  constructor() { }
+  @Input() track: TrackModel = { _id: 0, name: '', album: '', url: '', cover: '' };
+  constructor(private multimediaService: MultimediaService) { }
 
   ngOnInit(): void {
+  }
+  sendPlay(track: TrackModel): void {
+    console.log('Enviando canción al reproductor',track);
+    this.multimediaService.callback.emit(track);
   }
 
 }
